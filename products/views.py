a599bc8ca254e3acf2f16bf896e683edbff5e1a3
tanonkim <robbins4bos@gmail.com>
2022-03-03 21:00:07 +0900

    fix : for convention

--- products/views.py
+++ products/views.py
@@ -1,10 +1,7 @@
 import json, jwt
-from . import models
-
 
 from django.views     import View
 from django.http      import JsonResponse
-from django.views.generic import ListView
 
 from products.models  import Product, ProductSize
 from users.utils      import login_decorator
@@ -13,17 +10,17 @@ class ListView(View):
     @login_decorator
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            name = data.get("name", "")
-            description=data.get("description", "")
-            thumbnail = data.get("thumbnail", "")
+            data          = json.loads(request.body)
+            name          = data.get("name", "")
+            description   = data.get("description", "")
+            thumbnail     = data.get("thumbnail", "")
             discount_rate = data.get("discount_rate", "")
                 
             Product.objects.create(
-                name=name,
-                description=description,
-                thumbnail=thumbnail,
-                discount_rate=discount_rate
+                name          = name,
+                description   = description,
+                thumbnail     = thumbnail,
+                discount_rate = discount_rate
             )
             return JsonResponse({"MESSAGE": "List Created!"}, status=201)
         
@@ -31,21 +28,21 @@ class ListView(View):
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
 
     def get(self, request):
-        products   = Product.objects.filter()
-        productsize     = ProductSize.objects.filter()
-        results = []
+        products    = Product.objects.filter()
+        productsize = ProductSize.objects.filter()
+        results     = []
         
         for product in products:            
-                product_id = product.id
-                results.append(
-                    {
-                    "id" : product.id,
-                    "name" : product.name,
-                    "description" : product.description,
-                    "thumbnail" : product.thumbnail,
-                    "sizes" : product.sizes.all()[0].size,
-                    "discount_rate" : product.discount_rate,
-                    "price": productsize[product_id].price
+            product_id = product.id
+            results.append(
+                {
+                "id" : product.id,
+                "name" : product.name,
+                "description" : product.description,
+                "thumbnail" : product.thumbnail,
+                "sizes" : product.sizes.all()[0].size,
+                "discount_rate" : product.discount_rate,
+                "price": productsize[product_id].price
                 }
             )
         return JsonResponse({"lists" : results}, status = 200)        
