2cbbc4e1f462d15c61108831d452133089351189
Barney <holyja@naver.com>
2022-03-03 11:10:56 +0900

    add: SignInView in Views.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import SignUpView
+from users.views import SignUpView, SignInView
 
 urlpatterns = [
     path("/signup", SignUpView.as_view()),
+    path("/signin", SignInView.as_view()),
 ]
