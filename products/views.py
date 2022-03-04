9a57ede13e390b29baca685ae6184bc7395785e5
tanonkim <robbins4bos@gmail.com>
2022-03-05 01:51:18 +0900

    add : sorting for creation, pricing

--- products/views.py
+++ products/views.py
@@ -2,24 +2,39 @@ import json, jwt
 
 from django.views     import View
 from django.http      import JsonResponse
+from django.db.models import Q, F, Count , Max , Window
 
 from products.models  import Product, ProductSize
 
 class ProductsView(View):
     def get(self, request): 
-        products    = Product.objects.all()     
-        # product = [product for product in products]
-                   
-        results = [{
-            "id"            : product.id,
-            "name"          : product.name,
-            "description"   : product.description,
-            "thumbnail"     : product.thumbnail,
-            "sizes"         : product.sizes.all()[0].size,
-            "discount_rate" : product.discount_rate,
-            "price"         : ProductSize.objects.filter(product=product).first().price
-            } for product in products]
-        
+        try:
+            ordering = request.GET.get('ordering', None)
+            filter_set = {}            
                 
-        return JsonResponse({"lists" : results}, status = 200)
+            products = Product.objects.filter(**filter_set)
+            
+            if ordering == 'recent':
+                products = Product.objects.order_by('-created_at')
+                
+            results = [{
+                "id"            : product.id,
+                "name"          : product.name,
+                "description"   : product.description,
+                "thumbnail"     : product.thumbnail,
+                "sizes"         : product.sizes.all()[0].size,
+                "discount_rate" : float(product.discount_rate),
+                "price"         : int(ProductSize.objects.filter(product=product).first().price) * float(product.discount_rate)
+                } for product in products]
+            
+            if ordering == 'min_price':
+                results = sorted(results, key=lambda product: product['price'])
+            
+            if ordering == 'max_price':
+                results = sorted(results, key=lambda product: product['price'], reverse=True)
+   
+            
+            return JsonResponse({"lists" : results}, status = 200)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
                 
