0398e486962bd578b8e57dd1cde631adcc8f4a20
Barney <holyja@naver.com>
2022-03-04 20:15:05 +0900

    fix: modify detailView

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls    import path
+
 from products.views import ProductDetailView
 
 urlpatterns = [
-    path("/product/<id>", ProductDetailView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -2,62 +2,33 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
+
 from products.models  import Product
-from users.utils      import login_decorator
 
 class ProductDetailView(View):
-    @login_decorator
-    def post(self, request, id):
+    def get(self, request, product_id):
         try:
-            data               = json.loads(request.body)
-            product_id         = id
             product            = Product.objects.get(id = product_id)
-            product_images     = product.productimages.filter(product = product)
-            information_images = product.informationimages.filter(product = product)
-            product_urls       = []
-            information_urls   = []
+            product_images     = product.productimages.all()
+            information_images = product.informationimages.all()
+            product_sizes      = product.productsizes.all()
+            product_urls       = [image.url for image in product_images]
+            information_urls   = [information_image.url for information_image in information_images]
+            size_price         = [{'size' : product_size.size.size , 'price' : product_size.price} for product_size in product_sizes]
 
-            for information_image in information_images:
-                information_urls.append(information_image.url)
-
-            for image in product_images:
-                product_urls.append(image.url)
-
-            if data.get('size_id') == None:
-                sizes      = product.sizes.all()
-                size_list  = []
-
-                for size in sizes:
-                    size_list.append(size.size)
-
-                result = {
-                    'descriptin'         : product.description,
-                    'name'               : product.name,
-                    'price'              : product.productsizes.filter(product_id = product_id, size_id = 1)[0].price,
-                    'size'               : size_list,
-                    'discount_rate'      : product.discount_rate,
-                    'product_images'     : product_urls,
-                    'information_images' : information_urls
-                }
-
-                return JsonResponse({"message" : result})
-
-            if  data.get('size_id'):
-                size_id = data['size_id']
-                result = {
-                    'descriptin'         : product.description,
-                    'name'               : product.name,
-                    'price'              : product.productsizes.filter(product_id = product_id, size_id = size_id)[0].price,
-                    'size'               : product.productsizes.filter(product_id = product_id, size_id = size_id)[0].size.size,
-                    'discount_rate'      : product.discount_rate,
-                    'product_images'     : product_urls,
-                    'information_images' : information_urls
-                }
-
-                return JsonResponse({"message" : result}, status = 200)
+            result = {
+                'description'        : product.description,
+                'name'               : product.name,
+                'size_price'         : size_price,
+                'discount_rate'      : product.discount_rate,
+                'product_images'     : product_urls,
+                'information_images' : information_urls
+            }
+            
+            return JsonResponse({"message" : result}, status = 200)
 
         except Product.DoesNotExist:
-            return JsonResponse({"message" : "INVALID_PRODUCT"})
+            return JsonResponse({"message" : "INVALID_PRODUCT"}, status = 404)
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
