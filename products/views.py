148d0987fddaf7b7fab262561aab724c592f7544
tanonkim <robbins4bos@gmail.com>
2022-03-07 00:52:50 +0900

    fix : sort_set

--- products/views.py
+++ products/views.py
@@ -9,22 +9,22 @@ class ProductsView(View):
         try:
             ordering = request.GET.get('ordering', None)
             sort     = request.GET.get('sort', "id")
-            size     = request.GET.get('size', None).split(',')
-            page     = int(request.GET.get('page', 0))
+            size     = request.GET.get('size', 0)
+            offset     = int(request.GET.get('offset', 0))
             limit    = int(request.GET.get('limit', 8))
             
             q = Q()
                 
             if size:
+                size = size.split(',')
                 q &= Q(sizes__in=size)
             
             sort_set = {
                 "id" : "id",
+                'recent': '-created_at',
+                'old': 'created_at',
             } 
                       
-            if ordering == 'recent':
-                results = Product.objects.order_by('-created_at')
-                
             results = [{
                 "id"             : product.id,
                 "name"           : product.name,
@@ -34,7 +34,8 @@ class ProductsView(View):
                 "discount_rate"  : float(product.discount_rate),
                 "price"          : int(ProductSize.objects.filter(product=product).first().price), 
                 "discount_price" : int(ProductSize.objects.filter(product=product).first().price) * float(product.discount_rate)
-                } for product in Product.objects.filter(q).distinct().order_by(sort_set[sort])[page:page+limit]]
+                #"all_sizes"      : [ProductSize.objects.filter(product=product).first().size.size for in]
+                } for product in Product.objects.filter(q).distinct().order_by(sort_set[sort])[offset:offset+limit]]
                 
             if ordering == 'min_price':
                 results = sorted(results, key=lambda product: product['discount_price'])
