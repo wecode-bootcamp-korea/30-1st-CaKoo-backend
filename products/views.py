38acd8ea4309e5767fbd972ffead3a954afde215
Barney <holyja@naver.com>
2022-03-06 23:28:29 +0900

    fix: modify detailView base_price

--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http      import JsonResponse
 from django.views     import View
 
@@ -9,17 +7,24 @@ class ProductDetailView(View):
     def get(self, request, product_id):
         try:
             product            = Product.objects.get(id = product_id)
-            product_images     = product.productimages.all()
-            information_images = product.informationimages.all()
             product_sizes      = product.productsizes.all()
-            product_urls       = [image.url for image in product_images]
-            information_urls   = [information_image.url for information_image in information_images]
-            size_price         = [{'size' : product_size.size.size , 'price' : product_size.price} for product_size in product_sizes]
+            product_urls       = [image.url for image in product.productimages.all()]
+            information_urls   = [information_image.url for information_image in product.informationimages.all()]
+            size_price         = [{
+                'size'  : product_size.size.size,
+                'price' : product_size.price
+                } for product_size in product.productsizes.all()]
+            
+            base_price = 0
+
+            for i in size_price:
+                if i['size'] == 'mini':
+                    base_price = i['price']
 
             result = {
                 'description'        : product.description,
                 'name'               : product.name,
-                'base_price'         : product_sizes[0].price,
+                'base_price'         : base_price,
                 'size_price'         : size_price,
                 'discount_rate'      : product.discount_rate,
                 'product_images'     : product_urls,
@@ -30,6 +35,3 @@ class ProductDetailView(View):
 
         except Product.DoesNotExist:
             return JsonResponse({"message" : "INVALID_PRODUCT"}, status = 404)
-
-        except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
