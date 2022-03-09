dd209a8a460a9fcb07e16e559523fb7e24a772cd
Barney <holyja@naver.com>
2022-03-09 21:30:49 +0900

    fix: modify order get api

--- orders/views.py
+++ orders/views.py
@@ -10,14 +10,16 @@ class OrderView(View):
         carts = Cart.objects.filter(user = request.user)
         
         result = [{
-            'name'       : cart.product_size.product.name,
-            'quantity'   : cart.quantity,
-            'price'      : int(cart.quantity * cart.product_size.price),
-            'thumnail'   : cart.product_size.product.thumbnail,
-            'user_name'  : cart.user.name,
-            'user_phone' : cart.user.phone_number
-        } for cart in carts]
-
-        result.append({'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts]))})
+            'user_name'   : request.user.name,
+            'user_phone'  : request.user.phone_number,
+            'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts])),
+            'items'       : [{
+                              'product_name' : cart.product_size.product.name,
+                              'quantity'     : cart.quantity,
+                              'price'        : int(cart.quantity * cart.product_size.price),
+                              'thumbnail'    : cart.product_size.product.thumbnail,
+                              'size'         : cart.product_size.size.size   
+            } for cart in carts]
+        }]
 
         return JsonResponse({"message" : result}, status = 200)
