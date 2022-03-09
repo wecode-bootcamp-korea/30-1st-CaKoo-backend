092ed95777185abf562d309e63e9e6ec8e3d9b35
tanonkim <robbins4bos@gmail.com>
2022-03-09 15:45:48 +0900

    add : order post api

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,5 @@ urlpatterns = [
     path("users", include('users.urls')),
     path("carts", include('carts.urls')),
     path("products", include('products.urls')),
-<<<<<<< HEAD
     path("orders", include('orders.urls'))
-=======
-    path("carts", include('carts.urls')),
->>>>>>> main
 ]
