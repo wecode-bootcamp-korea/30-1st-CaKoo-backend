c4e76193d9e018cc01c1f88bbad6564152dedae5
Barney <holyja@naver.com>
2022-03-10 00:21:03 +0900

    Feat: Add try-exept func
    OrderView get메소드에 try-exept 함수를 사용하여 tokken으로 불러온 user에 해당하는 cart가 존재하지 않을 시 예외처리로 카트가 없다는 내용을 response

--- orders/views.py
+++ orders/views.py
@@ -7,19 +7,23 @@ from carts.models import Cart
 class OrderView(View):
     @login_decorator
     def get(self, request):
-        carts = Cart.objects.filter(user = request.user)
-        
-        result = [{
-            'user_name'   : request.user.name,
-            'user_phone'  : request.user.phone_number,
-            'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts])),
-            'items'       : [{
-                              'product_name' : cart.product_size.product.name,
-                              'quantity'     : cart.quantity,
-                              'price'        : int(cart.quantity * cart.product_size.price),
-                              'thumbnail'    : cart.product_size.product.thumbnail,
-                              'size'         : cart.product_size.size.size   
-            } for cart in carts]
-        }]
+        try:
+            carts = Cart.objects.filter(user = request.user)
+            
+            result = [{
+                'user_name'   : request.user.name,
+                'user_phone'  : request.user.phone_number,
+                'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts])),
+                'items'       : [{
+                                'product_name' : cart.product_size.product.name,
+                                'quantity'     : cart.quantity,
+                                'price'        : int(cart.quantity * cart.product_size.price),
+                                'thumbnail'    : cart.product_size.product.thumbnail,
+                                'size'         : cart.product_size.size.size   
+                } for cart in carts]
+            }]
 
-        return JsonResponse({"message" : result}, status = 200)
+            return JsonResponse({"message" : result}, status = 200)
+        
+        except Cart.DoesNotExist:
+            return JsonResponse({"message" : "DOES_NOT_EXIST"})
