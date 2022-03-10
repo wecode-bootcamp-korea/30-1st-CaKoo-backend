3250ef575ed8e874157c07226d87febc66ae7100
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-10 14:16:48 +0900

    Mentor review

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 import json, uuid
+from enum import Enum
 
 from django.http   import JsonResponse
 from django.views  import View
@@ -8,6 +9,12 @@ from users.utils   import login_decorator
 from carts.models  import Cart
 from orders.models import Order,OrderItem, OrderStatus
 
+class OrderStatus(Enum):
+    CONFIRMED = 1
+    CANCLED   = 2
+    PENDDING  = 3
+
+
 class OrderView(View):
     @login_decorator
     def post(self, request):
@@ -15,20 +22,20 @@ class OrderView(View):
             data = json.loads(request.body)
             user = request.user
             
-            cart_id      = data["cart_id"].split(',')
-            carts        = Cart.objects.filter(user=user, id__in=cart_id)
-            order_status = OrderStatus.objects.get(status="Confirmed") 
+            cart_ids     = data["cart_ids"]
+            carts        = Cart.objects.filter(user=user, id__in=cart_ids)
+            order_status = OrderStatus.objects.get(status=OrderStatus.CONFIRMED.value) 
 
             with transaction.atomic():
                 order = Order.objects.create(
-                        user            = user, 
-                        sender_name     = user.name,
-                        order_number    = uuid.uuid4(),
-                        order_status    = order_status,
-                        address         = data["address"],
-                        recipient_name  = data["recipient_name"],
-                        recipient_phone = data["recipient_phone"],
-                    )
+                    user            = user, 
+                    sender_name     = user.name,
+                    order_number    = uuid.uuid4(),
+                    order_status    = order_status,
+                    address         = data["address"],
+                    recipient_name  = data["recipient_name"],
+                    recipient_phone = data["recipient_phone"],
+                )
             
                 order_items = [
                     OrderItem(
