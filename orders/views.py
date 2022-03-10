c06755502356112ae7f43f3777fb0e5c250d34ad
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-10 14:47:55 +0900

    Mentor review

--- orders/views.py
+++ orders/views.py
@@ -2,28 +2,43 @@ from django.http  import JsonResponse
 from django.views import View
 
 from users.utils  import login_decorator
-from carts.models import Cart
+from orders.models import Order
 
 class OrderView(View):
     @login_decorator
     def get(self, request):
         try:
-            carts = Cart.objects.filter(user = request.user)
+            user   = request.user
+            orders = Order.objects.filter(user=user)
             
+            # result = [{
+            #     'user_name'   : user.name,
+            #     'user_phone'  : user.phone_number,
+                # 'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts])),
+            #     'items'       : [{
+            #         'product_name' : order.product_size.product.name,
+            #         'quantity'     : cart.quantity,
+            #         'price'        : int(cart.quantity * cart.product_size.price),
+            #         'thumbnail'    : cart.product_size.product.thumbnail,
+            #         'size'         : cart.product_size.size.size   
+            #     } for cart in orders]
+            # }]
+
             result = [{
-                'user_name'   : request.user.name,
-                'user_phone'  : request.user.phone_number,
-                'total_price' : int(sum([cart.product_size.price * cart.quantity for cart in carts])),
-                'items'       : [{
-                                'product_name' : cart.product_size.product.name,
-                                'quantity'     : cart.quantity,
-                                'price'        : int(cart.quantity * cart.product_size.price),
-                                'thumbnail'    : cart.product_size.product.thumbnail,
-                                'size'         : cart.product_size.size.size   
-                } for cart in carts]
+                "user_name" : user.name, 
+                "data" : [{
+                    "order_id" : order.id,
+                    "order_number" : order.order_number,
+                    "items" : [{
+                        "product_id" : order_item.product_size.product.id,
+                        "product_name" : order_item.product_size.product.name,
+                        "size" : order_item.product_size.size.size,
+                        "price" : order_item.product_sie.price
+                    }for order_item in order.orderitems.all()]
+                }for order in orders]
             }]
 
             return JsonResponse({"message" : result}, status = 200)
         
-        except Cart.DoesNotExist:
+        except Order.DoesNotExist:
             return JsonResponse({"message" : "DOES_NOT_EXIST"})
